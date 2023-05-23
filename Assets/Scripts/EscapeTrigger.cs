40c4ec40b7c2e2cb03b77146c0b524c777151555
Kuklaket <smakova.2001.ru@gmail.com>
2023-05-23 19:04:51 +0300

    Create EscapeTrigger.cs

---
+++ Assets/Scripts/EscapeTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class EscapeTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject _rogue;
+    [SerializeField] private GameObject _alex;
+
+    [SerializeField] private Rogue _actionRogue;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Rogue>(out Rogue rogue))
+        {
+            _alex.SetActive(true);
+            _actionRogue.ChangeDirection();
+        }
+    }
+}
