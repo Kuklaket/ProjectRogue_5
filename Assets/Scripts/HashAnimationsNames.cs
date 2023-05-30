6ef9fd84092ad880a4ee6a35778ab0b5610e5171
Kuklaket <smakova.2001.ru@gmail.com>
2023-05-30 11:36:59 +0300

    Create HashAnimationsNames.cs

---
+++ Assets/Scripts/HashAnimationsNames.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashAnimationsNames
+{
+    public static int IsHackAsHash = Animator.StringToHash("IsHack");
+    public static int IsOpenAsHash = Animator.StringToHash("IsOpen");
+}
