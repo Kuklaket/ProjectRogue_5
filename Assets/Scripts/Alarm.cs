34f3595449403da9f4aa7a827e8a3c13a286a2f7
Kuklaket <smakova.2001.ru@gmail.com>
2023-06-02 08:45:26 +0300

    Update Alarm.cs

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -8,8 +8,6 @@ public class Alarm : MonoBehaviour
     [SerializeField] private AudioSource _sos;
 
     private Coroutine _upVolume;
-    private Coroutine _downolume;
-
     private float _volumeMinimum = 0.3f;
     private float _volumeMaximum = 0.9f;
     private bool _isOn = false;
@@ -24,15 +22,12 @@ public class Alarm : MonoBehaviour
             _sos.Play();
             _animator.SetBool(HashAnimationsNames.IsHackAsHash, true);
 
-            StopNoNullCoroutine(_downolume);
-
             _upVolume = StartCoroutine(ChangeVolume(_volumeMaximum));
         }
         else
         {
             StopNoNullCoroutine(_upVolume);
-
-            _downolume = StartCoroutine(ChangeVolume(_volumeMinimum));
+            StartCoroutine(ChangeVolume(_volumeMinimum));
         }
     }
 
